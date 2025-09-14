da1ca68d04f21303cd97c3958a7289819015b12e
Swapnil <swapnil.ravi10@gmail.com>
2025-09-14 17:44:44 +0530

    Completed feature for list project, env and version

--- gitenvy/cli.py
+++ gitenvy/cli.py
@@ -41,7 +41,12 @@ def init(repo, path):
 @click.option('--env','--env-name', required=True, help='Environment name (e.g., dev, prod)')
 @click.option("--repo-path", default=DEFAULT_REPO_PATH, help="Local git repo path to store encrypted envs")
 def push(project, env, repo_path):
-    """Push and encrypt the .env file for a specific project and environment."""
+    """
+    Push .env file for a specific project and environment. \n
+    Usage: \n
+        gitenvy push --project <PROJECT> --env <ENV> 
+            Encrypts and pushes the .env file in the current directory to the specified project and environment.
+    """
     manager = EnvManager(project, env, repo_path=repo_path)
     try:
         out_file = manager.push()
@@ -50,34 +55,87 @@ def push(project, env, repo_path):
         click.echo(str(e))
 
 @cli.command()
-@click.option("--project", required=True)
-@click.option("--env", "--env-name", required=True)
+@click.option("--project", required=False, help="Project name")
+@click.option("--env", "--env-name", required=False, help="Environment name")
+@click.option("--v","--version", required=False, help="Version")
 @click.option("--repo-path", default=DEFAULT_REPO_PATH)
 def list(project, env, repo_path):
-    """List all versions of a project/env"""
-    base_dir = os.path.join(repo_path, project, env)
-    if not os.path.exists(base_dir):
-        click.echo("No versions found")
-        return
+    """
+    List projects, envs, or versions. \n
+    Usage: \n
+        gitenvy list 
+            List all projects.
 
-    versions = sorted([v for v in os.listdir(base_dir) if v.isdigit()])
-    for v in versions:
-        meta_file = os.path.join(base_dir, v, "metadata.json")
-        if os.path.exists(meta_file):
-            with open(meta_file) as f:
-                meta = json.load(f)
-            click.echo(f"Version {v}: updated by {meta['last_updated_by']} at {meta['last_updated_at']}")
-        else:
-            click.echo(f"Version {v}: metadata missing")
+        gitenvy list --project <PROJECT>
+            List all environments under the specified project.
+
+        gitenvy list --project <PROJECT> --env <ENV>
+            List all versions for the specified environment in the project.
+
+        gitenvy list --project <PROJECT> --env <ENV> --version
+            List details of all versions
+
+        gitenvy list --project <PROJECT> --env <ENV> --version <VERSION>
+            List details of specific version (e.g., 1)
+
+        gitenvy list --project <PROJECT> --env <ENV> --version latest
+            List details of latest version (e.g., latest)
+
+    Returns:
+        Prints a list of projects, environments, or version metadata depending on the options provided.
+    """
+    manager = EnvManager(project or  "", env or "", repo_path=repo_path)
+    try:
+        if not project and not env:
+            projects = manager.list_projects()
+            if not projects:
+                click.echo("⚠️ No projects found")
+                return
+            else:
+                click.echo("📁 Projects:")
+                for p in projects:
+                    click.echo(f" - {p}")
+                return
+        elif project and not env:
+            envs = manager.list_envs(project)
+            if not envs:
+                click.echo(f"⚠️ No environments found for project {project}")
+                return
+            else:
+                click.echo(f"📁 Environments for project {project}:")
+                for e in envs:
+                    click.echo(f" - {e}")
+                return
+        elif project and env:
+            versions = manager.list_versions()
+            if not versions:
+                click.echo("⚠️ No versions found")
+                return
+            for v in versions:
+                if v["last_updated_by"] and v["last_updated_at"]:
+                    click.echo(f"📄 Version {v['version']}: updated by {v['last_updated_by']} at {v['last_updated_at']}")
+                else:
+                    click.echo(f"📄 Version {v['version']}: metadata missing")
+    except Exception as e:
+        click.echo(f"⚠️ {str(e)}")
+    
 
 @cli.command()
 @click.option("--project", required=True)
 @click.option("--env", "--env-name", required=True)
 @click.option("--version", default="latest", help="Version to pull")
-@click.option("--out-path", default=".env", help="Where to save decrypted .env")
+@click.option("--out-path", default=".env", help="Where to save decrypted .env on machine")
 @click.option("--repo-path", default=DEFAULT_REPO_PATH)
 def pull(project, env, version, out_path, repo_path):
-    """Pull a version of .env from git and decrypt it locally"""
+    """
+    Pull .env from git or a specific project and environment. \n
+    Usage: \n
+        gitenvy pull --project <PROJECT> --env <ENV> 
+            Pulls and decrypts the .env file for the specified project and environment. Default version is 'latest' and output path is './.env'.
+
+        gitenvy pull --project <PROJECT> --env <ENV> --version <VERSION> --out-path <PATH>
+            Optionally specify a version (default is 'latest') and output path (default is './.env').
+    """
     manager = EnvManager(project, env, repo_path=repo_path)
     try:
         file_path = manager.pull(version=version, out_path=out_path)
